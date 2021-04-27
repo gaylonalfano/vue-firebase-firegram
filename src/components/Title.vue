64fedaed97cc01b44260b82a567e789975205942
gaylonalfano <gaylon.alfano@gmail.com>
2021-04-27 16:12:55 +0800

    Initial commit. Vue 3 CLI Babel TS Jest.

---
+++ src/components/Title.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="title">
+    <h1>FireGram</h1>
+    <h2>Your Pictures</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Title",
+});
+</script>
